269bf0e2829eb7f2b9f4d6379b31f44dbbd284cf
Zane Whitfield <zwhitfield@salesforce.com>
2020-04-13 18:18:45 -0700

    WIP creating request object and using server to check request

--- force-app/main/default/lwc/stockSummary/stockSummary.js
+++ force-app/main/default/lwc/stockSummary/stockSummary.js
@@ -1,9 +1,12 @@
-import { LightningElement, wire,  api } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import { NavigationMixin } from 'lightning/navigation';
 
+//importing test server for request objects
+import { myExpressApp } from '../testRequestObjects/app';
+
 /** Wire adapter to load records, utils to extract values. */
-import { getRecord,getFieldValue } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 /** Account Schema. */
 //import ACCOUNT_OBJECT from '@salesforce/schema/Account';
@@ -21,56 +24,71 @@ export default class StockSummary extends LightningElement {
     recordId = '0015w000026xyh3AAA';
     //@api objectApiName;
     //@api recordId;    
-     
+
     //accountObject = ACCOUNT_OBJECT;
     //fields = ['TickerSymbol'];
-    
+
     @wire(CurrentPageReference) pageRef;
-    @wire(getRecord, { recordId: '$recordId', fields: [TICKER_FIELD] }) 
+    @wire(getRecord, { recordId: '$recordId', fields: [TICKER_FIELD] })
     account;
-    
-    
+
+
     get ticker_symbol() {
         //return this.account.data ? getFieldValue(this.account.data, TICKER_FIELD) : ' ';
         return getFieldValue(this.account.data, TICKER_FIELD);
-   
+
     }
-/*
-    var query = querystring.stringify({symbol: 'AAPL'});
-    const requesturl = url.parse(url.format({
-        protocol: 'https',
-        hostname: 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-        pathname: '/stock/v2/get-summary',
-        query: {
-            symbol: AAPL
+    /*
+        var query = querystring.stringify({symbol: 'AAPL'});
+        const requesturl = url.parse(url.format({
+            protocol: 'https',
+            hostname: 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+            pathname: '/stock/v2/get-summary',
+            query: {
+                symbol: AAPL
+            }
+        }));
+    */
+
+    queryData = ticker_symbol();
+    myInit = {
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            "x-rapidapi-key": "fed0dc9471msh19edb801e26a30ep1cba77jsn002db7d60898"
         }
-    }));
-*/
-   
+    };
+
+
+    //request object
+    requestObj = new Request(this.queryData, this.myInit);
+
+
 
     connectedCallback() {
-       
-        fetch ('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=AAPL',
-        // End point URL
-        {
-            // Request type
-            method:"GET",
 
-            headers:{// content type
-                     "Content-Type": "application/json",
-                     "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-                     "x-rapidapi-key": "fed0dc9471msh19edb801e26a30ep1cba77jsn002db7d60898"
-            },
-            "hostname": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-        }
-        ).then(function(response){
+        fetch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=AAPL',
+            // End point URL
+            {
+                // Request type
+                method: "GET",
+
+                headers: {// content type
+                    "Content-Type": "application/json",
+                    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "fed0dc9471msh19edb801e26a30ep1cba77jsn002db7d60898"
+                },
+                "hostname": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+            }
+        ).then(function (response) {
             return response.json();
         }).then(() => {
             // console.log('%%%%'+JSON.stringify(myJson));
         }
 
         )
-    
+
     }
-    
+
 }
--- force-app/main/default/lwc/testRequestObjects/app.js
+++ force-app/main/default/lwc/testRequestObjects/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('returned data from request object'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
