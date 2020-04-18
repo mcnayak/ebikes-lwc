2a1d60101e721639d021ba1f01c7362bd6e4ea91
“mcnayak” <cmohan.nayak@gmail.com>
2020-04-17 17:19:31 -0700

    encodedTicker parameterized not working

--- force-app/main/default/lwc/stockSummary/stockSummary.js
+++ force-app/main/default/lwc/stockSummary/stockSummary.js
@@ -13,15 +13,14 @@ import TICKER_FIELD from '@salesforce/schema/Account.TickerSymbol';
 
 
 /** Record fields to load */
-// const fields = [TICKER_FIELD];
-// const querystring = require('querystring');
-// const url = require('url');
+
+
 export default class StockSummary extends LightningElement {
     @track 
-    Margins;
+    profit_Margins;
     @track 
-    Employees;
-    
+    fullTime_Employees;
+  
     
 
     recordId = '0015w000026xyh3AAA';
@@ -38,48 +37,17 @@ export default class StockSummary extends LightningElement {
     
     @api
     get tickerSymbol() {
-        //return this.account.data ? getFieldValue(this.account.data, TICKER_FIELD) : ' ';
-        return getFieldValue(this.account.data, TICKER_FIELD);
         
-   
+        return getFieldValue(this.account.data, TICKER_FIELD);
     }
-/*
-    var query = querystring.stringify({symbol: 'AAPL'});
-    const requesturl = url.parse(url.format({
-        protocol: 'https',
-        hostname: 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-        pathname: '/stock/v2/get-summary',
-        query: {
-            symbol: AAPL
-        }
-    }));
-*/
-/*
-var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary");
-
-req.query({
-	"region": "US",
-	"symbol": "AMRN"
-});
-
-req.headers({
-	"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-	"x-rapidapi-key": "fed0dc9471msh19edb801e26a30ep1cba77jsn002db7d60898"
-});
 
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
 
-	console.log(res.body);
-});
-*/
     connectedCallback() {
-        const encodedTicker = 'AAPL'; 
+        const encodedTicker = 'AAPL'; // I want this to be based on the Account Page I am on
+        // However if I call this.ticketSymbol property I get undefined.
         
         fetch (`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${encodedTicker}`,
- //       fetch ('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=AAPL',
-        // End point URL
         {
             // Request type
             method:"GET",
@@ -97,11 +65,9 @@ req.end(function (res) {
             return response.json();
         }).then((myJson) => {
              //console.log('%%%%'+JSON.stringify(json));
-             this.Margins = JSON.stringify(myJson.financialData.profitMargins.fmt);
-             this.Employees = JSON.stringify(myJson.summaryProfile.fullTimeEmployees);
-             //this.profitMargins = new Stock(JSON.stringify(myJson.financialData.profitMargins.fmt),JSON.stringify(myJson.financialData.profitMargins.fmt));
-             //this.stockDetails = new Stock('29','3000');
-             
+             this.profit_Margins = JSON.stringify(myJson.financialData.profitMargins.fmt);
+             this.fullTime_Employees = JSON.stringify(myJson.summaryProfile.fullTimeEmployees);
+           
              //console.log('%% Profit Margins%% '+JSON.stringify(myJson.financialData.profitMargins.fmt));
              //console.log('%% Employees %% '+JSON.stringify(myJson.summaryProfile.fullTimeEmployees));
 
