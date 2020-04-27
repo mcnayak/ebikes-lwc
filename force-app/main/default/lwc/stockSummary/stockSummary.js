9f371f0018a101c7771b64d267ce5a944cc54d75
“mcnayak” <cmohan.nayak@gmail.com>
2020-04-27 15:17:01 -0700

    Component Visibility Working

--- force-app/main/default/lwc/stockSummary/stockSummary.js
+++ force-app/main/default/lwc/stockSummary/stockSummary.js
@@ -21,7 +21,7 @@ export default class StockSummary extends LightningElement {
     @track     recent_Quarter;
     @track     yearly_Revenue;
     @track     yearly_Earnings;
-
+    @track     tick_symbol;
     account;
     tick_symbol;
     
@@ -69,13 +69,13 @@ export default class StockSummary extends LightningElement {
              this.fullTime_Employees = JSON.stringify(myJson.summaryProfile.fullTimeEmployees);
              let years = myJson.earnings.financialsChart.yearly.length;
              this.ticker_Symbol = this.tick_symbol;
-             this.recent_Quarter = JSON.stringify(myJson.earnings.financialsChart.quarterly[3].date)+JSON.stringify(myJson.earnings.financialsChart.quarterly[3].revenue.fmt);
+             this.recent_Quarter = JSON.stringify(myJson.earnings.financialsChart.quarterly[3].date)+ '  ' + JSON.stringify(myJson.earnings.financialsChart.quarterly[3].revenue.fmt);
              this.yearly_Revenue = myJson.earnings.financialsChart.yearly[years-1].revenue.fmt;
              this.yearly_Earnings = myJson.earnings.financialsChart.yearly[years-1].earnings.fmt;
              console.log(myJson.earnings.financialsChart.yearly.length);
              console.log(' Profit Margins%% '+JSON.stringify(myJson.financialData.profitMargins.fmt));
              console.log('%% Employees %% '+JSON.stringify(myJson.summaryProfile.fullTimeEmployees));
-             console.log('%% Recent Quarter Revenue %% '+JSON.stringify(myJson.earnings.financialsChart.quarterly[3].date)+ ' ' + JSON.stringify(myJson.earnings.financialsChart.quarterly[3].revenue.fmt));
+             console.log('%% Recent Quarter Revenue %% '+JSON.stringify(myJson.earnings.financialsChart.quarterly[3].date)+ '  $' + JSON.stringify(myJson.earnings.financialsChart.quarterly[3].revenue.fmt));
              console.log('%% Annual Revenue %% '+ (myJson.earnings.financialsChart.yearly[years-1].revenue.fmt));
              console.log('%% Annual Earnings %% '+ (myJson.earnings.financialsChart.yearly[years-1].earnings.fmt));
 
